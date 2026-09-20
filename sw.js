f3f6aad370fab1b7b154bf60333dde6805392afb
alishaWary <waryalisha1@gmail.com>
2026-09-20 13:50:30 +0530

    Update cache version to 'strike-terminal-v2'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'strike-terminal-v1';
+const CACHE = 'strike-terminal-v2';
 const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
